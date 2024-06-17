6be78065025e5ee3dc27404df2c2876be53c7f90
juani-g16 <56902964+juani-g16@users.noreply.github.com>
2024-06-17 16:27:44 +0000

    Added user log in check.

--- clouduploader.sh
+++ clouduploader.sh
@@ -1,16 +1,22 @@
 #! /bin/bash
 
 setup() {
-    # Install az cli
-    if ! $(command -v az) >/dev/null 2>&1
+    # Install az cli if it's not already installed
+    if ! command -v az >/dev/null 2>&1
     then
         echo "Azure CLI could not be found"
         echo "Installing Azure CLI"
         curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
     else
-        echo "Azure CLI found, proceeding to log in"
-        az login --use-device-code
-        echo "You're logged in."
+        echo "Azure CLI already installed" 
+        #Check whether there's an user logged in or proceed to log in otherwise
+        if az ad signed-in-user show 2>&1 | grep -q "ERROR"; then 
+            echo "Logged off, please log in"
+            az login --use-device-code
+            echo "You're now logged in"
+        else
+            echo "Already logged in"
+        fi
     fi
     # Login
     
